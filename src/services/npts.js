911f4c69a73060e3de8eba877e291e4371db6ddd
Jonathan Barrow <jonbarrow1998@gmail.com>
2021-10-17 12:29:04 -0400

    fixed task sheets

--- src/services/npts.js
+++ src/services/npts.js
@@ -7,9 +7,9 @@ const subdomain = require('express-subdomain');
 const npts = express.Router();
 
 // Setup routes
-npts.get('/p01/tasksheet/1/:id/:fileName', (request, response) => {
+npts.get('/p01/tasksheet/1/:id/:hash/:fileName', (request, response) => {
 	const { id, fileName } = request.params;
-	const tasksheetPath = path.normalize(`${__dirname}/../../cdn/tasksheet/${id}/${fileName}`);
+	const tasksheetPath = path.normalize(`${__dirname}/../../cdn/tasksheet/${id}/${hash}/${fileName}`);
 
 	if (fs.existsSync(tasksheetPath)) {
 		response.sendFile(tasksheetPath);
