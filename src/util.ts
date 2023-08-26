799c2f5b78c121241a1e830f361d36f2735fe582
Jonathan Barrow <jonbarrow1998@gmail.com>
2023-08-25 22:34:15 -0400

    Added many more countries and languages

--- src/util.ts
+++ src/util.ts
@@ -26,11 +26,21 @@ const gRPCAccountChannel = createChannel(`${config.grpc.account.address}:${confi
 const gRPCAccountClient: AccountClient = createClient(AccountDefinition, gRPCAccountChannel);
 
 const VALID_COUNTRIES = [
-	'US', 'JP', 'CR', 'FR'
+	'GB', 'US', 'IT', 'NL', 'DE', 'CA', 'FR', 'HU', 'CR',
+	'AU', 'BR', 'RO', 'CL', 'MX', 'RU', 'ES', 'JP', 'CZ',
+	'PT', 'MT', 'AR', 'SE', 'PL', 'IE', 'BE', 'HT', 'NO',
+	'FI', 'GR', 'BO', 'AT', 'VE', 'PA', 'PE', 'GF', 'SA',
+	'CO', 'LT', 'NA', 'CH', 'CY', 'RS', 'KY', 'GP', 'DK',
+	'KR', 'LU', 'SV', 'VA', 'GT', 'SK', 'HR', 'ZA', 'DO',
+	'UY', 'LV', 'HN', 'JM', 'TR', 'IN', 'ER', 'AW', 'NZ',
+	'EC', 'TW', 'EE', 'CN', 'SI', 'AI', 'BG', 'NI', 'IS',
+	'MQ', 'BZ', 'BA', 'MY', 'AZ', 'ZW', 'AL', 'IM', 'VG',
+	'VI', 'BM', 'GY', 'SR', 'MS', 'TC', 'BB', 'TT'
 ];
 
 const VALID_LANGUAGES = [
-	'en', 'ja'
+	'en', 'it', 'de', 'fr', 'es', 'us', 'pt', 'ru', 'ja',
+	'nl', 'ko', 'zh', 'tw'
 ];
 
 const VALID_FILE_TYPES = [
